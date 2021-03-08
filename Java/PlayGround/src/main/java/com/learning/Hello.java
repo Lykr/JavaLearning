584480d023926fd2d28107eefc0bbbffb53c0ed9
Lykr <674634684@qq.com>
2021-03-08 17:34:41 +0800

    Update Playground

--- Java/PlayGround/src/main/java/com/learning/Hello.java
+++ Java/PlayGround/src/main/java/com/learning/Hello.java
@@ -1,6 +1,18 @@
 package com.learning;
 
 public class Hello {
+    static {
+        System.out.println("Hello class static block.");
+    }
+
+    {
+        System.out.println("Hello class non-static block.");
+    }
+
+    public Hello() {
+        System.out.println("Hello class constructor.");
+    }
+
     public static void hello() {
         System.out.println("Hello!");
     }
---
+++ Java/PlayGround/src/main/java/com/learning/StaticBlock.java
@@ -0,0 +1,8 @@
+package com.learning;
+
+public class StaticBlock {
+    public static void main(String[] args) throws Exception {
+        Class hello = Class.forName("com.learning.Hello"); // 只要加载了，就会执行静态代码块
+        Object o = hello.getDeclaredConstructor().newInstance(); // 初始化后时，先执行非静态代码块，再执行构造方法
+    }
+}
