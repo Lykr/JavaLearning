9215e278c8d3208ccee7edf8df546448cde53248
Lykr <674634684@qq.com>
2021-04-03 14:44:23 +0800

    Add HeapSort

--- Java/PlayGround/src/main/java/com/learning/algorithm/sort/HeapSort.java
+++ Java/PlayGround/src/main/java/com/learning/algorithm/sort/HeapSort.java
@@ -21,13 +21,20 @@ public class HeapSort {
         }
 
         for (int i = 0; i < topK; i++) {
-            // Adjust heap
-            adjust(nums, 0,n - i);
             // Swap nums
             swap(nums, 0, n - i - 1);
+            // Adjust heap
+            adjust(nums, 0,n - i - 1);
         }
     }
 
+    /**
+     * Put number at i to appropriate position for ensuring
+     * {@code nums[k] >= nums[2 * k + 1] && nums[k] >= nums[2 * k + 2]} is true
+     * @param nums data
+     * @param i root idx
+     * @param len length of sorted part
+     */
     private static void adjust(int[] nums, int i, int len) {
         for (int k = 2 * i + 1; k < len; k = 2 * k + 1) {
             if (k + 1 < len && nums[k + 1] > nums[k]) k++;
