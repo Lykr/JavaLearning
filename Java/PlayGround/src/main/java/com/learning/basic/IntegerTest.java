da60b2d8ca549d3d2dc08cc17c3678dc0c5d72e3
Chen Wang <674634684@qq.com>
2021-04-14 17:52:47 +0800

    Update

---
+++ Java/PlayGround/src/main/java/com/learning/basic/IntegerTest.java
@@ -0,0 +1,9 @@
+package com.learning.basic;
+
+public class IntegerTest {
+    public static void main(String[] args) {
+        int a = 40;
+        Integer boxedA = new Integer(40);
+        System.out.println(a == boxedA);
+    }
+}
